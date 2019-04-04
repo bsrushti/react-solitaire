04a92236c27acd0af5175fbba455aa8c6206951e
bsrushti <bsrushti@in-bsrushti.local>
2019-04-04 11:38:02 +0530

    Getting one card from card stocks after clicking on pile.
    getting spade cards only.
    getting repetative cards from pile.
    
    Introduces following classes :
    - Game
    - Pile
    - Spade

--- src/App.js
+++ src/App.js
@@ -1,19 +1,95 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class App extends Component {
+class Spade extends Component {
+  getCard(number) {
+    return (
+      <div className="card">
+        <div className="header">{number}</div>
+        <div className="symbol">&#9824;</div>
+        <div className="footer">{number}</div>
+      </div>
+    );
+  }
+}
+
+class Pile extends Component {
   constructor(props) {
     super(props);
+    this.suits = {
+      spades: new Spade()
+    };
   }
 
   renderPile() {
-    return <div className="card face-down-card" />;
+    return (
+      <div className="card face-down-card" onClick={this.putCard.bind(this)} />
+    );
+  }
+
+  getRandomNumber() {
+    return Math.round(Math.random() * 10);
+  }
+
+  getOneCard() {
+    let number = this.getRandomNumber();
+    return this.suits.spades.getCard(number);
+  }
+
+  render() {
+    return <div className="card face-down-card" onClick={this.props.onClick} />;
+  }
+}
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.pile = new Pile();
+    this.state = {
+      card: <div id="pile-card" className="card" />
+    };
+  }
+
+  renderPile() {
+    return this.pile.renderPile();
+  }
+
+  getOneCard() {
+    this.setState({ card: this.pile.getOneCard() });
+  }
+
+  renderCard() {
+    return <Pile onClick={this.getOneCard.bind(this)} />;
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="pile">
+          <div>{this.renderCard()}</div>
+          <div>{this.state.card}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      game: <Game />
+    };
+  }
+
+  renderGame() {
+    return this.state.game;
   }
 
   render() {
     return (
       <div className="App">
-        <div>{this.renderPile()}</div>
+        <div className="container">{this.renderGame()}</div>
       </div>
     );
   }
