b4bd7292321bbf9830494ee1384999d29afa93d1
bsrushti <bsrushti@in-bsrushti.local>
2019-04-04 18:53:25 +0530

    Displaying four foundation, cards are not able to stack on foundation

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Pile from "./Pile";
+import Card from "./suit/Card";
 
 class Game extends Component {
   constructor(props) {
@@ -10,25 +11,33 @@ class Game extends Component {
     };
   }
 
-  renderPile() {
-    return this.pile.renderPile();
-  }
-
   getOneCard() {
     this.setState({ card: this.pile.getOneCard() });
   }
 
-  renderCard() {
+  renderPile() {
     return <Pile onClick={this.getOneCard.bind(this)} />;
+  }
+
+  renderFoundation() {
+    return (
+      <div className="four-foundation">
+        <Card classes="card" />
+        <Card classes="card" />
+        <Card classes="card" />
+        <Card classes="card" />
+      </div>
+    );
   }
 
   render() {
     return (
       <div className="container">
         <div className="pile">
-          <div>{this.renderCard()}</div>
-          <div>{this.state.card}</div>
+          <div title="pile">{this.renderPile()}</div>
+          <div title="face-up-card-from-pile">{this.state.card}</div>
         </div>
+        <div title="four-foundations">{this.renderFoundation()}</div>
       </div>
     );
   }
