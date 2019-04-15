c10d4b382c6c6a2fe1b200de15e9584005e09daa
bsrushti <bsrushti@in-bsrushti.Dlink>
2019-04-15 22:46:35 +0530

    Updates

---
+++ precache-manifest.407762a82c5fb89d468ea14b047d7c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5f27bd4358d7c2054cc",
+    "url": "/react-solitaire/static/css/main.c427f747.chunk.css"
+  },
+  {
+    "revision": "a5f27bd4358d7c2054cc",
+    "url": "/react-solitaire/static/js/main.996bcbb2.chunk.js"
+  },
+  {
+    "revision": "8c1a7d75fef5984be1e8",
+    "url": "/react-solitaire/static/js/runtime~main.7ea19e75.js"
+  },
+  {
+    "revision": "4ff2db79b538dd95c5bc",
+    "url": "/react-solitaire/static/js/2.b30af6b9.chunk.js"
+  },
+  {
+    "revision": "0e77e582f63d858e481666050a83e379",
+    "url": "/react-solitaire/index.html"
+  }
+];
